f87f7cd78eb8455a234fca4bbad992a59b0a820e
Raunak Regmi <raunak.regmi@gmail.com>
2024-08-20 13:40:14 +0545

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import models.Conversion;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Conversion distance = new Conversion(10);
+        System.out.println(distance.getKilometers() + " Kilometers");
+        System.out.println(distance.getCentimeters() + " Centimeters");
+        System.out.println(distance.getMillimeters() + " Millimeters");
+        System.out.println(distance.getDecimeters() + " Decimeters");
     }
 }
--- src/models/Conversion.java
+++ src/models/Conversion.java
@@ -1,2 +1,25 @@
-package models;public class Conversion {
+package models;
+
+public class Conversion {
+    private Integer baseValue;
+
+    public Conversion(Integer baseValue) {
+        this.baseValue = baseValue;
+    }
+
+    public int getCentimeters(){
+        return baseValue * 100;
+    }
+
+    public int getKilometers(){
+        return baseValue / 100;
+    }
+
+    public int getMillimeters(){
+        return baseValue * 1000;
+    }
+
+    public int getDecimeters(){
+        return baseValue * 10;
+    }
 }
